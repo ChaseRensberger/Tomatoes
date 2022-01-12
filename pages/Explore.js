4c042b687ff60297bdf422b34a9005d404621238
stella.masucci <stella.masucci@gmail.com>
2022-01-12 16:08:33 -0500

    tabs, home/explore pages created

---
+++ pages/Explore.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Explore = () => {
+  return (
+    <SafeAreaView>
+        <Text>Explore</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Explore;

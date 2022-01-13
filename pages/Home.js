157adcb68ad7a1691c3e4d3256954613fc40c815
stella.masucci <stella.masucci@gmail.com>
2022-01-13 10:04:54 -0500

    recent articles headline

--- pages/Home.js
+++ pages/Home.js
@@ -7,7 +7,8 @@ const Home = () => {
 
     <SafeAreaView>
         <ScrollView>
-
+            <Text style={{fontFamily:'-apple-system, BlinkMacSystemFont, "Segoe UI"', fontSize:24,paddingTop:15,
+    paddingLeft:20, color: '#1E136C'}}>Recent Articles</Text>
         </ScrollView>
     </SafeAreaView>
   );
